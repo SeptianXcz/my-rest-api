c711d0dac0cb541ed4d35b50106a3f7ad4beccb5
SeptianXcz <109943864+SeptianXcz@users.noreply.github.com>
2023-06-02 02:55:58 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Yamada Ryosuke'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://ryosukeofficialwb:konfirmasi@cluster0.nnaohyp.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@oik/hea(uwvaux(vx/@" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "septiannhdnata555@gmail.com" //email
+global.email_password = "konfirmasi" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
